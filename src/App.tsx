86d380de75e12bc4b8056622166e213c6c339bef
kzoeps <kzoepa@gmail.com>
2022-08-11 11:52:35 +0600

    feat(home): add home page, router and chakraui

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { ChakraProvider } from '@chakra-ui/react'
+import {ChakraProvider} from '@chakra-ui/react';
+import {BrowserRouter} from 'react-router-dom';
+import HomePage from './pages/Home';
 
 
 function App() {
-  return (
+	return (
 		<ChakraProvider>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+			<BrowserRouter>
+				<HomePage/>
+			</BrowserRouter>
 		</ChakraProvider>
-  );
+	);
 }
 
 export default App;
