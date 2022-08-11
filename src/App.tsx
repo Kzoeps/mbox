667ffa439775c2db90fc3912d0ba37ff973414c8
kzoeps <kzoepa@gmail.com>
2022-08-11 11:45:20 +0600

    feat: add stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ChakraProvider } from '@chakra-ui/react'
+
 
 function App() {
   return (
+		<ChakraProvider>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -20,6 +23,7 @@ function App() {
         </a>
       </header>
     </div>
+		</ChakraProvider>
   );
 }
 
