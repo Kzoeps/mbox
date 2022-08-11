6f3323705bf21878cb66009253c010908abf2738
kzoeps <kzoepa@gmail.com>
2022-08-11 11:57:27 +0600

    feat(nav): add navigation and chakra ui icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import './App.css';
 import {ChakraProvider} from '@chakra-ui/react';
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import HomePage from './pages/Home';
+import Navigation from './components/navigation';
 
 
 function App() {
 	return (
 		<ChakraProvider>
 			<BrowserRouter>
-				<HomePage/>
+				<Navigation/>
+				<Routes>
+					<Route path={'/'} element={<HomePage/>}/>
+				</Routes>
 			</BrowserRouter>
 		</ChakraProvider>
 	);
