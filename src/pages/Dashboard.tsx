1b5997738db257a6c42461521a0af2156a53a6b7
kzoeps <kzoepa@gmail.com>
2022-08-24 19:33:00 +0600

    feat(mbox): get relevant info

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,18 +1,78 @@
-import React, {useRef} from 'react';
+import React, {ChangeEvent, useRef, useState} from 'react';
 import {Box, SimpleGrid} from '@chakra-ui/react';
 import {TbCameraPlus} from 'react-icons/tb';
 import {AiOutlineFileAdd} from 'react-icons/ai';
 import {MdOutlineViewList} from 'react-icons/md';
 import StatsCard from '../components/stats-card';
+import {NumString} from '../utils/util.types';
+import Fuse from 'fuse.js';
 
 export interface DashboardProps {
 }
 
+export type DetectionResponse = ((NumString[])[])[]
+
 export const Dashboard = (props: DashboardProps) => {
 	const inputRef = useRef<HTMLInputElement | null>(null);
-	const handleAdditionClick = () => {
-		inputRef.current?.click();
-	}
+	const [file, setFile] = useState<File | null | undefined>(null);
+	const handleAdditionClick = async () => {
+		await inputRef.current?.click();
+	};
+
+	const extractText = (data: DetectionResponse): string[] => {
+		return data.map((element) => element?.[1]?.[0]) as string[];
+	};
+
+	const findJournalNumber = (data: string[], journalIndex: number) => {
+		const possibilities: { value: NumString, distance: number }[] = [];
+		data.forEach((piece, index) => {
+			if (!isNaN(+piece)) {
+				possibilities.push({
+					value: piece,
+					distance: Math.abs(journalIndex - index)
+				});
+			}
+		});
+		if (!possibilities.length) {
+			return data[journalIndex + 1];
+		}
+		return possibilities.sort(({distance: firstDistance}, {distance: secondDistance}) => {
+			if (firstDistance > secondDistance) {
+				return 1;
+			} else if (firstDistance < secondDistance) {
+				return -1;
+			} else {
+				return 0;
+			}
+		})[0].value;
+	};
+
+	const findRelevantInfo = (data: string[]): { journalNumber: NumString, cost: string } => {
+		const searcher = new Fuse(data);
+		const result = searcher.search('Jrnl');
+		const queriedResult = result[0].refIndex;
+		const journalNumber = findJournalNumber(data, queriedResult);
+		let cost = searcher.search('Nu.')?.[0]?.item;
+		if (cost) {
+			const justCost = cost.split(' ')?.[1];
+			cost = justCost ?? cost;
+		}
+		return {journalNumber, cost};
+	};
+
+	const handleFileSelection = async (event: ChangeEvent<HTMLInputElement>) => {
+		const uploadedFile = event?.target?.files?.[0];
+		setFile(event?.target?.files?.[0]);
+		if (uploadedFile) {
+			const formData = new FormData();
+			formData.append('file', uploadedFile);
+			const response = await fetch('https://hacket-mbox.ml', {method: 'POST', body: formData});
+			const data = await response.json();
+			const extractedData = extractText(data);
+			findRelevantInfo(extractedData);
+		}
+	};
+
 	return (
 		<>
 			<Box maxW="7xl" mx={'auto'} pt={5} px={{base: 2, sm: 12, md: 17}}>
@@ -30,7 +90,8 @@ export const Dashboard = (props: DashboardProps) => {
 						stat={'Add Record'}
 						icon={<AiOutlineFileAdd size={'3em'}/>}
 					/>
-					<input ref={inputRef} style={{display: 'none'}} type="file" accept="image/*" capture={true}/>
+					<input onChange={handleFileSelection} ref={inputRef} style={{display: 'none'}} type="file"
+						   accept="image/*" capture={true}/>
 				</SimpleGrid>
 			</Box>
 		</>
