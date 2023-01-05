3b8cb66a791964d34f464003f4834808b906526c
kzoeps <kzoepa@gmail.com>
2023-01-05 14:20:15 -0500

    fix: change capture to true

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -113,7 +113,7 @@ export const Dashboard = (props: DashboardProps) => {
 						icon={<AiOutlineFileAdd size={'3em'}/>}
 					/>
 					<input onChange={handleFileSelection} ref={inputRef} style={{display: 'none'}} type="file"
-						   accept="image/*" capture={false}/>
+						   accept="image/*" capture={true}/>
 					{isLoading && <div className={'spinner_overlay'}>
 						<Spinner
 							className={'spinner'}
