d640b24c2a019e19a1f20c848ffd29e36b865cb7
kzoeps <kzoepa@gmail.com>
2023-01-06 17:00:18 -0500

    feat: update mobile variations

--- src/pages/Demo.tsx
+++ src/pages/Demo.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import {Box, Flex} from '@chakra-ui/react';
+import {Box, Flex, useMediaQuery} from '@chakra-ui/react';
 
 export interface DemoProps {
 }
 
 export const Demo = (props: DemoProps) => {
+	const [isMobileish] = useMediaQuery('(max-width: 680px)')
 	return (
 		<><Flex align={'center'}>
 			<Box margin={'auto'} mt={'50px'}>
-				<iframe width="380" height="214" src="https://www.youtube.com/embed/ZKuPPIvcocs"
+				<iframe width={isMobileish ? '380' : '560'} height={isMobileish ? '214' : '315'} src="https://www.youtube.com/embed/ZKuPPIvcocs"
 						title="YouTube video player" frameBorder="0"
 						allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
 						allowFullScreen/>
