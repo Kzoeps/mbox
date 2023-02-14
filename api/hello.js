4ea5d98b31627def3bcee0b638b7478405a8b9e5
kzoeps <kzoepa@gmail.com>
2023-02-14 18:42:02 -0500

    add up some things

--- api/hello.js
+++ api/hello.js
@@ -1,3 +1,7 @@
-export default function test(req, res) {
+import { ImageAnnotatorClient } from "@google-cloud/vision"
+
+const client = new ImageAnnotatorClient();
+
+export default async (req, res) => {
   res.send(`Hello ${req.body.name}`)
 }
