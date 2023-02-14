3c160da2090edaf801dc809f9e5f553b58c2e182
kzoeps <kzoepa@gmail.com>
2023-02-14 18:53:07 -0500

    test

--- api/hello.js
+++ api/hello.js
@@ -1,7 +1,9 @@
 import { ImageAnnotatorClient } from "@google-cloud/vision"
+import { Buffer } from "buffer";
 
 const client = new ImageAnnotatorClient();
 
 export default async (req, res) => {
+  const fileBuffer = Buffer.from(req.body.image);
   res.send(`Hello ${req.body.name}`)
 }
