af8dd9c76a3d578e6cf5dfa3de37f40284e5a3f0
kzoeps <kzoepa@gmail.com>
2023-03-02 16:06:15 -0500

    feat: add change buttons

--- src/pages/Analytics.tsx
+++ src/pages/Analytics.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import AnalyticCard from "../components/analytic-card";
-import { Box, Text } from "@chakra-ui/react";
+import { Box, IconButton, Text } from "@chakra-ui/react";
 import {FcMoneyTransfer} from 'react-icons/fc';
 import {RiExchangeBoxLine} from 'react-icons/ri';
 import {IoPricetagsOutline} from 'react-icons/io5';
 import DropDown from "../components/dropdown";
+import {AiOutlineRight, AiOutlineLeft} from 'react-icons/ai';
 
 export default function Analytics() {
     console.log('test')
@@ -13,6 +14,8 @@ export default function Analytics() {
             <Box m='auto' maxW={'315px'} bg={'gray.100'} mt='20px' p='10px' borderRadius={'md'} display={'flex'} justifyContent={'space-between'}> 
                 <Text mt='5px' fontSize='lg'>Filter By: </Text>
                 <DropDown activeValue="Month" options={[{value: 'month', label: 'Month'}, {value: 'week', label: 'Week'}, {value: 'Day', label: 'Day'}]}/>
+                <IconButton icon={<AiOutlineLeft/>} colorScheme="orange" aria-label={"left-arrow"}/>
+                <IconButton icon={<AiOutlineRight />} colorScheme="orange" aria-label={"right-arrow"}/>
             </Box>
             <AnalyticCard title="Total transaction amount" amount="Nu. 130,000" date="Feb 1st - Feb 28th">
                 <FcMoneyTransfer size='2em' color='orange'/>
