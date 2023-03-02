bf87824ca675da1c58a931f7f1cde62aba30dd33
kzoeps <kzoepa@gmail.com>
2023-03-02 13:46:38 -0500

    feat: add analytics card ui

--- src/pages/Analytics.tsx
+++ src/pages/Analytics.tsx
@@ -1,8 +1,22 @@
 import React from "react";
+import AnalyticCard from "../components/analytic-card";
+import { Box } from "@chakra-ui/react";
+import {FcMoneyTransfer} from 'react-icons/fc';
+import {RiExchangeBoxLine} from 'react-icons/ri';
 
 export default function Analytics() {
     console.log('test')
     return <>
-        <h1>Hello WOrld</h1>
+        <Box m={'auto'} >
+            <AnalyticCard title="Total transactions this month" amount="Nu. 130,000" date="Feb 1st - Feb 28th">
+                <FcMoneyTransfer size='2em' color='orange'/>
+            </AnalyticCard> 
+            <AnalyticCard title="Transactions this month" amount="4000" date="Feb 1st - Feb 28th">
+                <RiExchangeBoxLine size='2em' color='orange'/>
+            </AnalyticCard> 
+            <AnalyticCard title="Transactions this month" amount="4000" date="Feb 1st - Feb 28th">
+                <RiExchangeBoxLine size='2em' color='orange'/>
+            </AnalyticCard> 
+        </Box>
     </>
 }
