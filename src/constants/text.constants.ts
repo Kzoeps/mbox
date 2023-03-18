7ee79490b12dadd36104c73d80ee9b7fa17f3ae0
kzoeps <kzoepa@gmail.com>
2023-03-18 14:03:32 -0400

    update texts

--- src/constants/text.constants.ts
+++ src/constants/text.constants.ts
@@ -1,6 +1,6 @@
 export const HomeTranslations = {
     EASY_TO_USE: "Easily Record Transactions!",
-    EASY_TO_USE_DESCRIPTION: `Quickly take down Journal Numbers and amount in seconds with just your camera. We automatically get all the important details and save it for you!Record your transactions in a few clicks and get a detailed analysis of your spending habits.`,
+    EASY_TO_USE_DESCRIPTION: `Quickly take down Journal Numbers and amount in seconds with just your camera. We automatically get all the important details and save it for you!`,
     QUICK_VIEW: "Quickly View Transactions!",
     QUICK_VIEW_DESCRIPTION: `Instantly look up all your transactions. You can also filter them according to their date!`,
     ANALYTICS: "Key Insights At Your Fingertips!",
