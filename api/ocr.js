b71f51bef0955db89fae5f89b2c91876ba6b507a
kzoeps <kzoepa@gmail.com>
2023-06-22 16:32:10 -0400

    feat: add auth guard for sf

--- api/ocr.js
+++ api/ocr.js
@@ -1,26 +1,42 @@
 import vision from "@google-cloud/vision";
 import { GoogleAuth } from "google-auth-library";
-import { initializeApp, getAuth } from "firebase-admin";
+import admin from "firebase-admin";
+import dayjs from "dayjs";
+import "dotenv/config";
 
 const credentials = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+const firebaseCreds = JSON.parse(process.env.MBOX_APPLICATION_CREDENTIALS);
 const auth = new GoogleAuth({ credentials });
-const app = initializeApp({ auth: auth });
-const firebaseAuth = getAuth(app);
+let firebaseAuth = undefined;
+const app = admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: firebaseCreds.project_id,
+    clientEmail: firebaseCreds.client_email,
+    privateKey: firebaseCreds.private_key.replace(/\\n/g, "\n"),
+  }),
+});
+firebaseAuth = admin.auth(app);
 const client = new vision.ImageAnnotatorClient({ auth });
 
 const analyze = async (req, res) => {
   try {
+    if(!req.body.token || !req.body.image){
+      res.status(400).send("Bad Request");
+      return;
+    }
     const decodedToken = await firebaseAuth.verifyIdToken(req.body.token);
-    console.log(decodedToken);
-    if (decodedToken.uid) {
+    if (decodedToken.uid && dayjs.unix(decodedToken.exp).isAfter(dayjs())) {
       const fileBuffer = Buffer.from(req.body.image, "base64");
       const [result] = await client.textDetection(fileBuffer);
       const response = {
         detection: result.textAnnotations,
       };
       res.send(response);
+    } else{
+      res.status(401).send("Unauthorized");
     }
   } catch (e) {
+    console.log(e);
     res.status(400).send(e);
   }
 };
